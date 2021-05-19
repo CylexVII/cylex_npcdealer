f40271fbc6de95e4bc8ba753c2ca0fae9c3124ce
C Y L E X <54693274+CylexVII@users.noreply.github.com>
2021-05-19 23:22:27 +0300

    Update en.lua

--- cylex_npcdealer/locales/en.lua
+++ cylex_npcdealer/locales/en.lua
@@ -1,4 +1,5 @@
 Locales['en'] = {
+    ['menu_default_title'] = "Choose item",
     ['not_enough_item'] = "You don't have enough item!",
     ['progress_cancel'] = "Cancelled!",
     ['on_sell'] = "You sold %s x %s for $%s",
