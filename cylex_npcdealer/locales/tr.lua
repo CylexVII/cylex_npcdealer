1d5d1f80bb4e956ff4abb42764cb25dc33de3986
C Y L E X <54693274+CylexVII@users.noreply.github.com>
2021-05-19 23:22:44 +0300

    Update tr.lua

--- cylex_npcdealer/locales/tr.lua
+++ cylex_npcdealer/locales/tr.lua
@@ -1,4 +1,5 @@
 Locales['tr'] = {
+    ['menu_default_title'] = "Eşya seç",
     ['not_enough_item'] = "Yeterli eşyan yok!",
     ['progress_cancel'] = "İptal edildi!",
     ['on_sell'] = "%sx adet %s eşyasını $%s karşılığında sattın.",
